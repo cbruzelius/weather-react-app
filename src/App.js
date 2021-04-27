bbe16b5f2e466160956da9e9b23a170af67333e1
cbruzelius <76457870+cbruzelius@users.noreply.github.com>
2021-04-26 21:01:38 -0700

    Source Code added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
         <h1>Weather Search</h1>
       <h2>Enter the name of the city below.</h2>
       <Weather />
+      <p className="Source-code">
+        <a href="https://github.com/cbruzelius/weather-react-app" target= "blank">Open Source Code </a>
+         Created by: Cari Bruzelius
+      </p>
       </header>
+      
     </div>
   );
 }
--- src/Temperature.js
+++ src/Temperature.js
@@ -16,7 +16,7 @@ export default function Temperature(props) {
     setHumidity(response.data.main.humidity);
     setWind(Math.round(response.data.wind.speed));
     setIcon(response.data.weather[0].icon);
-    setCity(response.data.name);
+    setCity (response.data.name);
   }
 
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=7810de17cb3e4ad4b94e69a98fbe80a0&units=metric`;
